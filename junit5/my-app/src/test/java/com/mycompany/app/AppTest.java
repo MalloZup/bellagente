47c942cc6d97a8d97a6a1eae013c8ae8e2b5500a
dmaiocchi <dmaiocchi@suse.com>
2017-06-10 19:03:39 +0200

    add some learning stuff for juniit with mvn

---
+++ junit5/my-app/src/test/java/com/mycompany/app/AppTest.java
@@ -0,0 +1,12 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+class FirstJUnit5Tests {
+
+    @Test
+    void myFirstTest() {
+        assertEquals(3, 1 + 1);
+    }
+
+}
